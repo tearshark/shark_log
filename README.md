c7ebac2a9a766c8ee45b5ce5db5f415425157893
tearshark <tearshark@163.com>
2020-05-18 22:21:34 +0800

    针对NanoLog做性能测试

--- README.md
+++ README.md
@@ -61,6 +61,33 @@
 然后获得跟本线程相关的 log_buffer，将 s_type 以及 变化部分的参数交给 log_buffer
 生成一条 log_info 描述的日志信息 log。然后通知日志格式化线程进行格式化并落地。<br>
 
+### Runtime Latency
+(以下测试数据，除shark_log之外，来自[NanoLog](https://github.com/PlatformLab/NanoLog))<br>
+Measured in nanoseconds and each cell represents the 50th / 99.9th tail latencies. The log messages used can be found in the [Log Message Map below](#Log-Messages-Map).
+
+| Message | shark_log | NanoLog | spdlog | Log4j2 | glog | Boost | ETW |
+|---------|:--------:|:--------:|:--------:|:--------:|:--------:|:--------:|:--------:|
+|staticString | 19/25| 7/37| 214/2546| 174/3364 | 1198/5968| 1764/3772| 161/2967|
+|stringConcat | 19/28| 7/36| 279/905| 256/25087 | 1212/5881| 1829/5548| 191/3365|
+|singleInteger | 18/24| 7/32| 268/855| 180/9305 | 1242/5482| 1914/5759| 167/3007|
+|twoIntegers | 19/24| 8/62| 437/1416| 183/10896 | 1399/6100| 2333/7235| 177/3183|
+|singleDouble | 19/23| 8/43| 585/1562| 175/4351 | 1983/6957| 2610/7079| 165/3182|
+|complexFormat | 19/24| 8/40| 1776/5267| 202/18207 | 2569/8877| 3334/11038| 218/3426|
+
+#### Log Messages Map
+
+Log messages used in the benchmarks above. *Italics* indicate dynamic log arguments.
+
+| Message ID | Log Message Used |
+|--------------|:--------|
+|staticString  | Starting backup replica garbage collector thread |
+|singleInteger | Backup storage speeds (min): *181* MB/s read |
+|twoIntegers   | buffer has consumed *1032024* bytes of extra storage, current allocation: *1016544* bytes |
+|singleDouble  | Using tombstone ratio balancer with ratio = *0.4* |
+|complexFormat | Initialized InfUdDriver buffers: *50000* receive buffers (*97* MB), *50* transmit buffers (*0* MB), took *26.2* ms |
+|stringConcat  | Opened session with coordinator at *basic+udp:host=192.168.1.140,port=12246* |
+
+
 这是一个心血来潮玩具级别的实现。接受批评指点交流，不接受嘲讽。<br>
 如果你发现了任何bug、有好的建议、或使用上有不明之处，可以提交到issue，也可以直接联系作者:
     
--- SharkLog.cpp
+++ SharkLog.cpp
@@ -23,13 +23,71 @@ void test_tick_performace()
 	fmt::print("now() cost time: {0} CPU cycles", dt);
 }
 
-int main()
+template<class _Callback>
+void benchmark_log_callback(const uint64_t freq, const char * name, const _Callback& cb)
 {
-	static_assert(std::is_same_v<decltype(shark_decval(3, 8.0f, "Hello World!")), log_info<int, float, const char*>>, "");
+    const size_t K = 50;
+    const size_t N = 2000;
 
-    shark_log_initialize(shark_log_stdfile_factory(), false, "C:/logs/mylog-{0:04d}{1:02d}{2:02d}-{3:02d}{4:02d}{5:02d}.{6}");
-    sharkl_info("this is a test for disassembly {} / {}", 1, 2.0);
+    std::atomic<uint64_t> total{ 0 }, mintick{ (std::numeric_limits<uint64_t>::max)() }, stick;
 
+    for (int j = 0; j < K; ++j)
+    {
+        for (int i = 0; i < N; ++i)
+        {
+            stick.store(__rdtsc());
+
+            cb();
+
+            uint64_t dt = __rdtsc() - stick.load();
+
+            total += dt;
+            mintick.store((std::min)(mintick.load(), dt));
+        }
+
+        shark_log_notify_format(nullptr);       //通知格式化线程有数据写入
+        std::this_thread::sleep_for(10ms);      //给格式化线程足够的时间落地，以便于取得更好的写入延迟。
+    }
+
+    auto avg = (total * 1000 / freq) / (K * N);
+    mintick = mintick * 1000 / freq;
+    fmt::print("{0} cost time: min={1} ns, avg={2} ns.\r\n", name, mintick, avg);
+
+    std::this_thread::sleep_for(200ms);
+}
+
+void benchmark_log()
+{
+    const uint64_t freq = _log_tick_freq();
+
+    benchmark_log_callback(freq, "staticString", []
+        {
+        sharkl_info("Starting backup replica garbage collector thread"); 
+        });
+
+    benchmark_log_callback(freq, "staticString", []
+        {
+            sharkl_info("Starting backup replica garbage collector thread");
+        });
+    benchmark_log_callback(freq, "singleInteger", [] {
+        sharkl_info("Backup storage speeds (min): {} MB/s read", 181);
+        });
+    benchmark_log_callback(freq, "twoIntegers", [] {
+        sharkl_info("buffer has consumed {} bytes of extra storage, current allocation: {} bytes", 1032024, 1016544); 
+        });
+    benchmark_log_callback(freq, "singleDouble", [] {
+        sharkl_info("Using tombstone ratio balancer with ratio = {}", 0.4); 
+        });
+    benchmark_log_callback(freq, "complexFormat", [] {
+        sharkl_info("Initialized InfUdDriver buffers: {} receive buffers ({} MB), {} transmit buffers ({} MB), took {} ms", 50000, 97, 50, 0, 26.2f); 
+        });
+    benchmark_log_callback(freq, "stringConcat", [] {
+        sharkl_info("Opened session with coordinator at basic+udp:host={}, port={}", "192.168.1.140", 12246);
+        });
+}
+
+void test_log()
+{
     std::atomic<uint64_t> dt{ 0 };
 
     const size_t K = 100;
@@ -68,9 +126,18 @@ int main()
         std::this_thread::sleep_for(10ms);      //给格式化线程足够的时间落地，以便于取得更好的写入延迟。
     }
 
-	const uint64_t freq = _log_tick_freq();
+    const uint64_t freq = _log_tick_freq();
     auto avg = (dt.load() * 1000 / freq) / (K * N * 15);
     fmt::print("\nlog cost avg time: {0} ns.", avg);
+}
 
+int main()
+{
+	static_assert(std::is_same_v<decltype(shark_decval(3, 8.0f, "Hello World!")), log_info<int, float, const char*>>, "");
+
+    shark_log_initialize(shark_log_stdfile_factory(), true, "C:/logs/mylog-{0:04d}{1:02d}{2:02d}-{3:02d}{4:02d}{5:02d}.{6}");
+    sharkl_info("this is a test for disassembly {} / {}", 1, 2.0);
+
+    benchmark_log();
 	shark_log_destroy();
 }
